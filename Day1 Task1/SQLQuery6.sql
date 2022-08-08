093cd53498537714ae26ae7257feaf04d4599bfa
abhi7083 <71085654+abhi7083@users.noreply.github.com>
2022-08-08 18:05:07 +0530

    Add files via upload

---
+++ Day1 Task1/SQLQuery6.sql
@@ -0,0 +1,56 @@
+create database Administration
+use Administration
+
+create table tblEmp
+(
+Empno int primary key,
+Ename varchar(15) not null,
+Job varchar(15) not null,
+MgrId int,
+Hiredate datetime,
+sal float,
+comm int,
+Deptno int)
+
+create table tblDept
+(
+Deptno int primary key foreign key references tbldept(deptno),
+Dname varchar(20),
+LOC varchar(15))
+
+
+
+sp_help tblEmp
+
+insert into tblEmp values(7369,'SMITH','CLERK',7902,1982-12-17,800,NULL,20),
+(7499,'ALLEN','SALESMAN',7698,1981-02-20,1600,300,30),
+(7521,'WARD','SALESMAN',7902,1981-02-22,1250,500,30),
+(7566,'JONES','MANAGER',7839,1981-04-02,2975,NULL,20),
+(7654,'MARTIN','SALESMAN',7698,1981-09-28,1250,1400,30),
+(7698,'BLAKE','MANAGER',7839,1981-05-01,2850,NULL,30),
+(7782,'CLARK','MANAGER',7839,1981-06-09,2450,NULL,10),
+(7788,'SCOTT','ANALYST',7566,1987-04-19,3000,NULL,20),
+(7839,'KING','PRESIDENT',NULL,1981-11-17,5000,NULL,10),
+(7844,'TURNER','SALESMAN',7698,1981-09-08,1500,0,30),
+(7876,'ADAMS','CLERK',7788,1987-05-23,1100,NULL,20),
+(7900,'JAMES','CLERK',7698,1981-12-03,950,NULL,30),
+(7902,'FORD','ANALYST',7566,1981-12-03,3000,NULL,20),
+(7934,'MILLER','CLERK',7782,1982-01-23,NULL,1300,10)
+
+insert into tblDept values(10,'ACCOUNTING','NEW YORK'),
+(20,'RESEARCH','DALLAS'),
+(30,'SALES','CHICAGO'),
+(40,'OPERATIONS','BOSTON')
+
+SELECT * FROM tblEmp
+SELECT * FROM tblDept
+
+--- add column
+alter table tblEmp
+add Age int
+
+---- delete column
+alter table tblEmp
+drop column Age
+
+SELECT * FROM tblEmp where Ename like '_[ar]%'
